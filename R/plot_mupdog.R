da4912821ccb620ad304a9f3080e9cc202611d8a
David Gerard <gerard.1787@gmail.com>
2018-01-02 16:48:16 -0600

    adds summary function for mupdog

--- R/plot_mupdog.R
+++ R/plot_mupdog.R
@@ -22,3 +22,29 @@ plot.mupdog <- function(x, index, ...) {
     ggplot2::guides(alpha=ggplot2::guide_legend(title="maxpostprob"))
   return(pl)
 }
+
+
+#' Provide some summaries from the output of \code{\link{mupdog}}.
+#'
+#' @param object A mupdog object.
+#' @param ... Not used.
+#'
+#' @return A list with the following elements:
+#' \itemize{
+#' \item \code{freq} A matrix with the frequency of the dosages (rows) for each SNP (columns).
+#' \item \code{mean_dosage} The posterior mean dosage of an individual (rows) for each SNP (columns).
+#' }
+#'
+#' @author David Gerard
+#'
+#' @export
+summary.mupdog <- function(object, ...) {
+  assertthat::assert_that(is.mupdog(object))
+  p           <- ncol(object$input$refmat)
+  freq_mat    <- apply(rbind(object$map_dosage, matrix(rep(0:object$input$ploidy, times = p), nrow = object$input$ploidy + 1)), 2, table) - 1
+  mean_dosage <- apply(sweep(object$postprob, MARGIN = 3, STATS = 0:object$input$ploidy, FUN = "*"), c(1, 2), sum)
+  return_list             <- list()
+  return_list$freq        <- freq_mat
+  return_list$mean_dosage <- mean_dosage
+  return(return_list)
+}
