bc359b68e87b50945109da8e691bc80c34c4470f
David Gerard <gerard.1787@gmail.com>
2018-01-04 10:35:18 -0600

    added some example data

---
+++ R/data.R
@@ -0,0 +1,31 @@
+#' Subset of individuals and SNPs from Uitdewilligen et al (2013).
+#'
+#' A list containing a matrix of reference counts, a matrix of total counts, and the ploidy level (4) of the species.
+#' This is a subset of the data from Uitdewilligen et al (2013).
+#'
+#' @format A list containing three objects. Two matrices and a numeric scalar:
+#' \describe{
+#'   \item{refmat}{A matrix of read counts containing the reference allele. The rows index the individuals and the columns index the SNPs.}
+#'   \item{sizemat}{A matrix of the total number of read counts. The rows index the individuals and the columns index the SNPs.}
+#'   \item{ploidy}{The ploidy level of the species (just 4).}
+#' }
+#'
+#' @references Uitdewilligen, J. G., Wolters, A. M. A., Bjorn, B., Borm, T. J., Visser, R. G., & van Eck, H. J. (2013). \href{https://doi.org/10.1371/journal.pone.0062355}{A next-generation sequencing method for genotyping-by-sequencing of highly heterozygous autotetraploid potato.} PLoS One, 8(5), e62355.
+#'
+#'
+#' @source \url{https://doi.org/10.1371/journal.pone.0062355}
+#'
+#' @seealso \code{\link{mupout}}: a mupdog fit of these data.
+#'
+"uitdewilligen"
+
+
+#' A mupdog fit of the \code{\link{uitdewilligen}} data.
+#'
+#' @format An object of class \code{\link{mupdog}}.
+#'
+#' @source The raw data that this was fit to can be found in \code{\link{uitdewilligen}}.
+#'
+#' @seealso \code{\link{uitdewilligen}}, \code{\link{plot.mupdog}}, \code{\link{summary.mupdog}}
+#'
+"mupout"
---
+++ tests/testthat/test_generics.R
@@ -0,0 +1,17 @@
+context("generics")
+
+test_that("summary.mupdog works", {
+  msum <- summary.mupdog(mupout)
+  byhand <- c(table(c(mupout$map_dosage[, 1], 0:mupout$input$ploidy)) - 1)
+  expect_equal(msum$freq[, 1], byhand)
+
+
+  byhand <- sum(mupout$postprob[1, 1, ] * 0:mupout$input$ploidy)
+  expect_equal(msum$mean_dosage[1, 1], byhand)
+}
+)
+
+test_that("plot.mupdog works", {
+  pl <- plot.mupdog(mupout, 1)
+}
+)
